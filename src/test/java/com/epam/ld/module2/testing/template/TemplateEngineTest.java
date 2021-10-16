eec994749095fa1d4da879fa807cf906704cb35f
Mikhail <mikhail_petrukhin@epam.com>
2021-10-17 00:38:39 +0600

    TemplateEngineTest.messageShouldBeNotNUll RED PART

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -1,4 +1,17 @@
 package com.epam.ld.module2.testing.template;
 
+
+import com.epam.ld.module2.testing.Client;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 public class TemplateEngineTest {
+
+    @Test
+    public void messageShouldBeNotNUll() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        String message = templateEngine.generateMessage(new Template(), new Client());
+        assertNotNull(message);
+    }
 }
