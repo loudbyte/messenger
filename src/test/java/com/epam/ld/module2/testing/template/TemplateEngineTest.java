224e6a1b30bd91be0a1f1a026afdebdaeab2e600
Mikhail <Mikhail_Petrukhin@epam.com>
2021-10-17 03:22:02 +0600

    Refactoring

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -2,6 +2,7 @@ package com.epam.ld.module2.testing.template;
 
 
 import com.epam.ld.module2.testing.Client;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.platform.commons.util.StringUtils;
 
@@ -10,16 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TemplateEngineTest {
 
+    private TemplateEngine templateEngine;
+
+    @BeforeEach
+    public void setUp() {
+        templateEngine = new TemplateEngine();
+    }
+
     @Test
     public void messageShouldBeNotNUll() {
-        TemplateEngine templateEngine = new TemplateEngine();
         String message = templateEngine.generateMessage(new Template(), new Client());
         assertNotNull(message);
     }
 
     @Test
     public void messageShouldNotBeBlank() {
-        TemplateEngine templateEngine = new TemplateEngine();
         String message = templateEngine.generateMessage(new Template(), new Client());
         assertTrue(StringUtils.isNotBlank(message));
     }
