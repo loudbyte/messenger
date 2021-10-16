788d98387bba3397901d2f89060a827412715c01
Mikhail <Mikhail_Petrukhin@epam.com>
2021-10-17 01:09:34 +0600

    TemplateEngineTest.messageShouldNotBeBlank RED PART

--- src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
+++ src/test/java/com/epam/ld/module2/testing/template/TemplateEngineTest.java
@@ -3,8 +3,10 @@ package com.epam.ld.module2.testing.template;
 
 import com.epam.ld.module2.testing.Client;
 import org.junit.jupiter.api.Test;
+import org.junit.platform.commons.util.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TemplateEngineTest {
 
@@ -13,5 +15,12 @@ public class TemplateEngineTest {
         TemplateEngine templateEngine = new TemplateEngine();
         String message = templateEngine.generateMessage(new Template(), new Client());
         assertNotNull(message);
+    }
+
+    @Test
+    public void messageShouldNotBeBlank() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        String message = templateEngine.generateMessage(new Template(), new Client());
+        assertTrue(StringUtils.isNotBlank(message));
     }
 }
