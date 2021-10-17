1d80030ff6e796797d915972795119097b267135
Mikhail <Mikhail_Petrukhin@epam.com>
2021-10-17 22:02:53 +0600

    MailServerTest.mailServerShouldSendMessageToConsole RED PART

--- src/test/java/com/epam/ld/module2/testing/MailServerTest.java
+++ src/test/java/com/epam/ld/module2/testing/MailServerTest.java
@@ -1,4 +1,53 @@
 package com.epam.ld.module2.testing;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MailServerTest {
+
+    private final InputStream systemIn = System.in;
+    private final PrintStream systemOut = System.out;
+
+    private ByteArrayInputStream testIn;
+    private ByteArrayOutputStream testOut;
+
+    @BeforeEach
+    public void setUpOutput() {
+        testOut = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(testOut));
+    }
+
+    private void provideInput(String data) {
+        testIn = new ByteArrayInputStream(data.getBytes());
+        System.setIn(testIn);
+    }
+
+    private String getOutput() {
+        return testOut.toString();
+    }
+
+    @AfterEach
+    public void restoreSystemInputOutput() {
+        System.setIn(systemIn);
+        System.setOut(systemOut);
+    }
+
+    @Test
+    public void mailServerShouldSendMessageToConsole() {
+        final String testString = "Subject: #{subject}\nBody: #{body}";
+        provideInput(testString);
+
+        MailServer mailServer = new MailServer();
+        mailServer.send("", testString);
+
+        assertEquals(testString, getOutput());
+    }
 }
